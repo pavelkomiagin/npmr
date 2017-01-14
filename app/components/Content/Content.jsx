899cd52fe142efcc10847ce56ff3ab3f449b113d
Pavel <pavel.komiagin@gmail.com>
2017-01-14 20:36:11 +0300

    Make all html code

--- app/components/Content/Content.jsx
+++ app/components/Content/Content.jsx
@@ -118,8 +118,159 @@ class Content extends Component {
             Install packages...
           </div>
         </div>
-        <div className={styles.dependenciesContainer}>
+        {/*<div className={cx(styles.dependenciesContainer, styles.cardView)}>
           {this.dependencies}
+        </div>*/}
+
+        <div className={cx(styles.dependenciesContainer, styles.tableView)}>
+          <div className={styles.packagesListTitle}>{`Packages (24)`}</div>
+
+          <table className={styles.packagesTable}>
+            <th>
+              <td className={styles.checkboxColumn}>
+                <input type="checkbox" name="selectAll"/>
+              </td>
+              <td>Package name</td>
+              <td>Current version</td>
+              <td>Wanted version</td>
+              <td>Latest version</td>
+              <td>Environment</td>
+            </th>
+            <tbody>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+              <tr className={styles.packageRow}>
+                <td className={styles.checkboxColumn}>
+                  <input type="checkbox" name="select"/>
+                </td>
+                <td>react</td>
+                <td>1.0.2</td>
+                <td>1.0.3</td>
+                <td>1.0.4</td>
+                <td>dev</td>
+              </tr>
+            </tbody>
+          </table>
+          {/*{this.dependencies}*/}
+          <div className={styles.packageInfo}>
+            <div className={styles.packageInfoTitle}>Additional info for selected package</div>
+            <div className={cx(styles.infoItem, styles.name)}>
+              <div className={cx(styles.infoItemText, styles.nameText)}>Name:</div>
+              <div className={cx(styles.infoItemValue, styles.nameValue)}>babel-preset-stage-0</div>
+            </div>
+            <div className={cx(styles.infoItem, styles.description)}>
+              <div className={cx(styles.infoItemText, styles.descriptionText)}>Description:</div>
+              <div className={cx(styles.infoItemValue, styles.descriptionValue)}>-</div>
+            </div>
+            <div className={cx(styles.infoItem, styles.author)}>
+              <div className={cx(styles.infoItemText, styles.authorText)}>Author:</div>
+              <div className={cx(styles.infoItemValue, styles.authorValue)}>-</div>
+            </div>
+            <div className={cx(styles.infoItem, styles.license)}>
+              <div className={cx(styles.infoItemText, styles.licenseText)}>License:</div>
+              <div className={cx(styles.infoItemValue, styles.licenseValue)}>-</div>
+            </div>
+            <div className={cx(styles.infoItem, styles.homepage)}>
+              <div className={cx(styles.infoItemText, styles.homepageText)}>Homepage:</div>
+              <div className={cx(styles.infoItemValue, styles.homepageValue)}>-</div>
+            </div>
+            <div className={cx(styles.infoItem, styles.repository)}>
+              <div className={cx(styles.infoItemText, styles.repositoryText)}>Repository:</div>
+              <div className={cx(styles.infoItemValue, styles.repositoryValue)}>-</div>
+            </div>
+            <div className={cx(styles.infoItem, styles.issues)}>
+              <div className={cx(styles.infoItemText, styles.issuesText)}>Issues:</div>
+              <div className={cx(styles.infoItemValue, styles.issuesValue)}>-</div>
+            </div>
+          </div>
         </div>
       </div>
     );
